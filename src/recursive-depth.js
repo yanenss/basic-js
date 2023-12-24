8a472ccd1a831f4c3c7cdb3e4b2a65a343441f1a
Yana Nazarenko <yananazarenko49@gmail.com>
2023-12-24 21:12:46 +0300

    feat: add new js function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, currentDepth = 1, maxDepth = 1) {
+    arr.forEach((element) => {
+      if (Array.isArray(element)) {
+        maxDepth = Math.max(maxDepth, this.calculateDepth(element, currentDepth + 1, maxDepth));
+      }
+    });
+    return currentDepth > maxDepth ? currentDepth : maxDepth;
   }
 }
 
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,34 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position ) {
+    if (typeof position !== 'number' || !Number.isInteger(position) ||
+        position <= 0 || position > this.chain.length) {
+      this.chain.length = 0;
+      throw new Error("You can't remove incorrect link!");
+        }
+      this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const finishedChain = this.chain.join('~~');
+    this.chain.length = 0; // Clear the chain
+    return finishedChain;
   }
 };
 
